f9fdf797e8be730225d1a42e806853fbde7ab23b
virotep <virotep@miamioh.edu>
2017-01-16 15:45:26 -0200

    Services Module

---
+++ Services/src/app/service/index.ts
@@ -0,0 +1,4 @@
+export { ServiceComponent } from './service.component';
+export { CmpAComponent } from './cmp-a.component';
+export { CmpBComponent } from './cmp-b.component';
+export { LogService } from './log.service';
